a5058a6c6a3aeee4e17337f19ecf415752e39f32
Telep <telepnef@yandex.ru>
2025-01-18 11:01:23 +0300

    practice Form Test

---
+++ src/test/java/PracticeForm.java
@@ -0,0 +1,69 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.selector.ByText;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class PracticeForm {
+    @BeforeAll
+    static void setUp() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    public void practiceFormTest() {
+        open("/automation-practice-form");
+
+        $("#firstName").setValue("Evgen");
+        $("#lastName").setValue("Pupkin");
+        $("#userEmail").setValue("pupkin@mail.com");
+        $x("//label[text()='Male']").click();
+        $("#userNumber").setValue("1234567890");
+
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOptionContainingText("May");
+        $(".react-datepicker__year-select").selectOptionContainingText("1945");
+        $x("//div[@class='react-datepicker__week']/div[text()='9']").click();
+
+        $("#subjectsContainer").click();
+        $("#subjectsInput").setValue("Art").pressEnter();
+        $x("//label[text()='Music']").click();
+        $("#uploadPicture").uploadFile(new File("src/test/resources/Screenshot.png"));
+        $("#currentAddress").setValue("Evgen str. Red Will");
+        $("#submit").scrollTo();
+
+        $("#state").click();
+        $x("//div[text()='Uttar Pradesh']").click();
+        $("#city").click();
+        $(byText("Agra")).click();
+        $("#submit").click();
+ /*
+        // примеры
+          $$("div").findBy(text("Agra")).click();
+         $x("//div[text()='Agra']").click();
+*/
+
+
+        // assertions table
+        $(".table").$(byText("Student Name")).parent().shouldHave(text("Evgen Pupkin"));
+        $(".table").$(byText("Student Email")).parent().shouldHave(text("pupkin@mail.com"));
+        $(".table").$(byText("Gender")).parent().shouldHave(text("Male"));
+        $(".table").$(byText("Mobile")).parent().shouldHave(text("1234567890"));
+        $(".table").$(byText("Date of Birth")).parent().shouldHave(text("9 May,1945"));
+        $(".table").$(byText("Subjects")).parent().shouldHave(text("Art"));
+        $(".table").$(byText("Hobbies")).parent().shouldHave(text("Music"));
+        $(".table").$(byText("Picture")).parent().shouldHave(text("Screenshot.png"));
+        $(".table").$(byText("Address")).parent().shouldHave(text("Evgen str. Red Will"));
+        $(".table").$(byText("State and City")).parent().shouldHave(text("Uttar Pradesh Agra"));
+
+        $("#closeLargeModal").click();
+    }
+}
