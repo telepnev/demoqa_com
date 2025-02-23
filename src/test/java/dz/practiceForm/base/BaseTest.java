19fd32401acf1704c903701f15e7ec18f85f55da
Telep <telepnef@yandex.ru>
2025-02-23 19:42:55 +0300

    new branch attach end

--- src/test/java/dz/practiceForm/base/BaseTest.java
+++ src/test/java/dz/practiceForm/base/BaseTest.java
@@ -5,6 +5,9 @@ import com.codeborne.selenide.Selenide;
 import dz.helpers.Attach;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 import static com.codeborne.selenide.Selenide.executeJavaScript;
 
@@ -15,6 +18,13 @@ public class BaseTest {
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
     }
 
     @AfterEach
