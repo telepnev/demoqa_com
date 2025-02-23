3697e9e7d59da5c610134e1935aa46411b7caa75
Telep <telepnef@yandex.ru>
2025-02-23 18:42:01 +0300

    new branch allure + remote

--- src/test/java/dz/practiceForm/base/BaseTest.java
+++ src/test/java/dz/practiceForm/base/BaseTest.java
@@ -10,9 +10,10 @@ import static com.codeborne.selenide.Selenide.executeJavaScript;
 public class BaseTest {
     @BeforeAll
     static void setUp() {
-        Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 
     @AfterEach
